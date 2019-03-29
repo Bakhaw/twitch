125e09c41554002cbfe7ed395481dcf3fc4010f5
Faissal <fhattou@simplon.co>
2019-03-29 18:23:31 +0100

    created API requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import API from './api';
 import UpdateApp from './components/UpdateApp';
 
 export default function App() {
+  const fetch = async () => {
+    console.log('fired');
+    const req = await API.getUserById('59584981');
+    console.log('done', req);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <header>
       <p>
