42a3e210038233ceb1a486989e72384b43831a52
Faissal <fhattou@simplon.co>
2019-03-29 19:35:36 +0100

    firebase setup and update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## React PWA Boilerplate
+### Twitch.tv remake using [Twitch API](https://dev.twitch.tv)
+
+### First version of this project is [here](https://github.com/Bakhaw/twitch-app)
---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageWrapper from '../../components/PageWrapper';
+
+function Home() {
+  return (
+    <PageWrapper>
+      <h1>Home page</h1>
+    </PageWrapper>
+  );
+}
+
+export default Home;
