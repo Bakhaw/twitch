a488b4acdef917db76aa35aceecda2dc7d9abdef
Faissal <fhattou@simplon.co>
2019-04-01 13:56:25 +0200

    removed <LeftMenu /> in router

--- src/Router/index.js
+++ src/Router/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../screens/Home';
 import TopGames from '../screens/TopGames';
-import LeftMenu from '../components/LeftMenu';
 import NavBar from '../components/NavBar';
 
 function Router() {
@@ -11,7 +10,6 @@ function Router() {
     <BrowserRouter>
       <>
         <NavBar />
-        <LeftMenu />
         <Switch>
           <Route path='/directory' component={TopGames} />
           <Route path='/' component={Home} />
