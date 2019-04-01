2efd39f00c76061555b1632ea81293350f66e8c4
Faissal <fhattou@simplon.co>
2019-04-01 13:55:31 +0200

    Add toggle theme button in NavBar and switch backgrounds

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,22 +1,82 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import { colors, layout } from '../../stylesheets';
+
+import ToggleThemeButton from './ToggleThemeButton';
+import { layout, usePalette } from '../../stylesheets';
+
+const Links = [
+  {
+    id: 0,
+    label: 'Découvrir',
+    to: '/'
+  },
+  {
+    id: 1,
+    label: 'Parcourir',
+    to: '/directory'
+  }
+];
 
 const Wrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
   position: fixed;
-  top: 0;
   left: 0;
   right: 0;
   height: ${layout.NavBar.height};
-  background: ${colors.purple.default};
+  background: ${props => props.colors.NavBar.background};
 `;
 
-function NavBar() {
+const NavLinks = styled.ul`
+  display: flex;
+  align-items: center;
+  height: ${layout.NavBar.height};
+  width: 100%;
+`;
+
+const NavLink = styled.li`
+  margin: 0 10px;
+  position: relative;
+  a {
+    color: ${({ colors, isActive }) =>
+      isActive ? colors.NavBar.NavLink.active : colors.NavBar.NavLink.default};
+    :hover {
+      color: ${props => props.colors.NavBar.NavLink.active};
+    }
+  }
+  ${({ isActive }) =>
+    isActive &&
+    `
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    transform: translateY(30px);
+    height: 5px;
+    width: 100%;
+    background: #fff;
+  }
+`}
+`;
+
+function NavBar({ location }) {
+  const colors = usePalette();
   return (
-    <Wrapper>
-      <p>NavBar</p>
+    <Wrapper colors={colors}>
+      <NavLinks>
+        {Links.map(({ id, label, to }) => (
+          <NavLink key={id} colors={colors} isActive={location.pathname === to}>
+            <Link to={to}>{label}</Link>
+          </NavLink>
+        ))}
+      </NavLinks>
+      <ToggleThemeButton />
     </Wrapper>
   );
 }
 
-export default NavBar;
+export default withRouter(NavBar);
