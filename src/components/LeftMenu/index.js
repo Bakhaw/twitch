885289156b2231abdee2a42be1c83028ddc8a943
Faissal <fhattou@simplon.co>
2019-04-01 12:12:35 +0200

    add use of usePalette() custom hook

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import { colors, layout } from '../../stylesheets';
+import { layout, usePalette } from '../../stylesheets';
 
 const Wrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  background: ${colors.purple.dark};
+  background: ${props => props.colors.purple.dark};
   margin-top: ${layout.NavBar.height};
   height: calc(100vh - ${layout.NavBar.height});
   width: ${layout.LeftMenu.width};
 `;
 
 function LeftMenu() {
-  return (
-    <Wrapper>
-      <p>Left menu</p>
-    </Wrapper>
-  );
+  const colors = usePalette();
+  return <Wrapper colors={colors} />;
 }
 
 export default LeftMenu;
--- src/components/PageWrapper/index.js
+++ src/components/PageWrapper/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { layout } from '../../stylesheets';
+import { layout, usePalette } from '../../stylesheets';
 
 const Wrapper = styled.main`
-  overflow: scroll;
+  overflow-y: scroll;
   position: absolute;
   bottom: 0;
   right: 0;
   height: ${`calc(100vh - ${layout.NavBar.height})`};
   width: ${`calc(100vw - ${layout.LeftMenu.width})`};
+  background: ${props => props.colors.wrapper.background};
+`;
+
+const ChildrenWrapper = styled.div`
+  padding: 50px 20px;
 `;
 
 function PageWrapper({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  const colors = usePalette();
+  return (
+    <Wrapper colors={colors}>
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+    </Wrapper>
+  );
 }
 
 export default PageWrapper;
