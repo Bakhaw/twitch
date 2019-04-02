80c50e63d3b8d10da72ad90f6d5a2ab43674f92d
Faissal <fhattou@simplon.co>
2019-04-02 18:48:22 +0200

    created a function to get streamer avatar

--- src/screens/Streams/index.js
+++ src/screens/Streams/index.js
@@ -1,58 +1,75 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 
-import API from '../../api';
+import Banner from './Banner';
+import List from '../../components/List';
+import Loader from '../../components/Loader';
 import PageWrapper from '../../components/PageWrapper';
 import StreamCard from '../../components/StreamCard';
+import API from '../../api';
+import { useFetch } from '../../api/hooks';
 import { layout } from '../../stylesheets';
 
-const StreamsList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, ${layout.StreamCard.width}px);
-  justify-content: center;
-  gap: 40px;
-`;
-
-export default function Streams({ match }) {
-  const { gameId } = match.params;
+function Streams({ match }) {
   const [streams, setStreams] = useState([]);
+  const [maxObjects, setMaxObjects] = useState(20);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const getData = async () => {
-    const { data } = await API.getStreamsByGameId(gameId);
-    await getStreamerImages(data);
+  const { gameId } = match.params;
+  const { data: dataStreams } = useFetch('getStreamsByGameId', [
+    gameId,
+    maxObjects
+  ]);
+
+  const getData = async data => {
+    await setIsLoading(true);
+    if (data.length > 0) {
+      let streamersIds = '';
+      data.forEach(({ user_id }, index) => {
+        streamersIds += index === 0 ? `${user_id}` : `&id=${user_id}`;
+      });
+
+      const { data: dataUsers } = await API.getMultipleUsersById(streamersIds);
+      const newStreams = [...data];
+
+      dataUsers.forEach(({ profile_image_url }, index) => {
+        newStreams[index] = {
+          ...newStreams[index],
+          img: profile_image_url
+        };
+      });
+      await setStreams(newStreams);
+    }
+    await setIsLoading(false);
   };
 
-  const getStreamerImages = async data => {
-    let streamersIds = '';
-    data.forEach(({ user_id }, index) => {
-      streamersIds += index === 0 ? `${user_id}` : `&id=${user_id}`;
-    });
-
-    const request = await API.getMultipleUsersById(streamersIds);
-    const newStreams = [...data];
-    request.data.forEach(({ profile_image_url }, index) => {
-      newStreams[index] = {
-        ...newStreams[index],
-        img: profile_image_url
-      };
-    });
-
-    await setStreams(newStreams);
+  const onBottomReached = () => {
+    // ? this ternary is checking if we already got 100 objects, which is the max
+    // ? we can fetch using Twitch API
+    setMaxObjects(prevState => (prevState < 100 ? prevState + 20 : prevState));
   };
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData(dataStreams);
+  }, [dataStreams, maxObjects]);
 
   return (
-    <PageWrapper isLoading={streams.length === 0}>
-      <StreamsList>
-        {streams.map(stream => (
-          <li key={stream.id}>
-            <StreamCard stream={stream} />
-          </li>
-        ))}
-      </StreamsList>
-    </PageWrapper>
+    <>
+      <Banner gameId={gameId} />
+      <PageWrapper isLoading={streams.length === 0}>
+        <List
+          columnWidth={layout.StreamCard.width}
+          onBottomReached={onBottomReached}
+        >
+          {streams.map(stream => (
+            <li key={stream.id}>
+              <StreamCard stream={stream} />
+            </li>
+          ))}
+        </List>
+        {isLoading && <Loader />}
+      </PageWrapper>
+    </>
   );
 }
+
+export default Streams;
