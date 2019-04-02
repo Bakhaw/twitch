6aa8677faecd933a87a1d6dc3c21de1de4591c3e
Faissal <fhattou@simplon.co>
2019-04-02 18:47:42 +0200

    add <Banner /> to <Streams /> screen

---
+++ src/screens/Streams/Banner.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import GameCard from '../../components/GameCard';
+import Loader from '../../components/Loader';
+import { useFetch } from '../../api/hooks';
+import { usePalette } from '../../stylesheets';
+
+const Wrapper = styled.div`
+  background-image: ${props => props.colors.StreamsBanner.background},
+    url(${props => props.backgroundImage});
+  background-position: center;
+  background-size: cover;
+  padding: 35px 25px;
+  height: 200px;
+  & .GameCard {
+    cursor: unset;
+    display: flex;
+    &:hover .GameCard__Title {
+      text-decoration: unset;
+    }
+    .GameCard__Title {
+      color: ${props => props.colors.StreamsBanner.title};
+      font-size: 40px;
+      font-weight: 500;
+      text-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+      margin-left: 10px;
+      width: 100%;
+    }
+  }
+`;
+
+function Banner({ gameId }) {
+  const { data: game } = useFetch('getGameById', [gameId]);
+
+  if (game.length === 0) return <Loader />;
+
+  const colors = usePalette();
+  const backgroundImage = `${game[0].box_art_url.slice(0, -21)}-1920x1080.jpg`;
+  return (
+    <Wrapper backgroundImage={backgroundImage} colors={colors}>
+      <GameCard game={game[0]} />
+    </Wrapper>
+  );
+}
+
+export default Banner;
