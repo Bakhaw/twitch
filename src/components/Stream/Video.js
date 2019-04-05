56f5e925d8116bffc1a90f5778aa05788c9a43e1
bakhaw <fhattou@simplon.co>
2019-04-06 00:00:45 +0200

    cleaned <Video /> design

--- src/components/Stream/Video.js
+++ src/components/Stream/Video.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100%;
+  margin: 20px 0;
+`;
 
 const Video = ({ channel }) => (
-  <iframe
-    allowFullScreen
-    frameBorder={0}
-    scrolling='no'
-    src={`https://player.twitch.tv/?channel=${channel.login}`}
-    title={`${channel.login} live stream video`}
-  />
+  <Wrapper>
+    <iframe
+      allowFullScreen
+      frameBorder={0}
+      scrolling='no'
+      src={`https://player.twitch.tv/?channel=${channel.login}`}
+      title={`${channel.login} live stream video`}
+    />
+  </Wrapper>
 );
 
 Video.propTypes = {
