b9b2ecef44c00a9006e5c3adcfc1ee47acc9bc8f
bakhaw <fhattou@simplon.co>
2019-04-06 00:01:06 +0200

    cleaned <Chat /> design

--- src/components/Stream/Chat.js
+++ src/components/Stream/Chat.js
@@ -2,25 +2,36 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { layout } from '../../stylesheets';
+import { layout, usePalette } from '../../stylesheets';
 import { StateContext } from '../../Context';
 
 const Wrapper = styled.div`
-  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.26), 0 5px 5px rgba(0, 0, 0, 0.26);
+  padding: 40px 20px;
   width: ${layout.Chat.width}px;
+  iframe,
+  object,
+  embed {
+    box-shadow: ${props => props.colors.LiveStream.boxShadow};
+    border: 5px solid rgba(255, 255, 255, 0);
+    -webkit-border-radius: 6px !important;
+    -ms-border-radius: 6px !important;
+    -o-border-radius: 6px !important;
+    border-radius: 6px !important;
+  }
 `;
 
 const Chat = ({ channel }) => {
+  const colors = usePalette();
   const { currentTheme } = useContext(StateContext);
   return (
-    <Wrapper>
+    <Wrapper colors={colors}>
       <iframe
         allowFullScreen
         frameBorder={0}
-        id={channel}
+        id={channel.login}
         src={`https://www.twitch.tv/embed/${
           channel.login
-        }/chat/${currentTheme === 'Dark' && '?darkpopout'}`}
+        }/chat/${currentTheme === 'Dark' && '?darkpopout'}&wmode=transparent`}
         title={`${channel.login} live stream video`}
       />
     </Wrapper>
