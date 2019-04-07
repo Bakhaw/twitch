d26fc95c2f3c46b4a3fbe7a84326bfd8393a06b9
Faissal <fhattou@simplon.co>
2019-04-07 21:07:13 +0200

    use { useFetch } on <Videos />

--- src/screens/Videos/index.js
+++ src/screens/Videos/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import List from '../../components/List';
@@ -8,12 +8,27 @@ import { layout } from '../../stylesheets';
 import { useFetch } from '../../api/hooks';
 
 function Videos({ match }) {
+  const [maxObjects, setMaxObjects] = useState(20);
+
   const { userId } = match.params;
-  const { data: videos } = useFetch('getVideosByParam', ['user_id', userId]);
+  const { data: videos } = useFetch('getVideosByParam', [
+    'user_id',
+    userId,
+    maxObjects
+  ]);
+
+  const onBottomReached = () => {
+    // ? this ternary is checking if we already got 100 objects, which is the max
+    // ? we can fetch using Twitch API
+    setMaxObjects(prev => (prev < 100 ? prev + 20 : prev));
+  };
 
   return (
     <PageWrapper isLoading={videos.length === 0}>
-      <List columnWidth={layout.StreamCard.width}>
+      <List
+        columnWidth={layout.StreamCard.width}
+        onBottomReached={onBottomReached}
+      >
         {videos.map(video => (
           <li key={video.id}>
             <StreamCard stream={video} />
