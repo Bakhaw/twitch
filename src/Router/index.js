0f527db950ae3f09dc194e4d4902771a5d449fde
Faissal <fhattou@simplon.co>
2019-04-07 20:52:20 +0200

    added React.lazy and Suspense

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Home from '../screens/Home';
-import LiveStream from '../screens/LiveStream';
-import LiveStreams from '../screens/LiveStreams';
-import TopGames from '../screens/TopGames';
-import Videos from '../screens/Videos';
+import Loader from '../components/Loader';
 import NavBar from '../components/NavBar';
+
+const Home = lazy(() => import('../screens/Home'));
+const LiveStream = lazy(() => import('../screens/LiveStream'));
+const LiveStreams = lazy(() => import('../screens/LiveStreams'));
+const TopGames = lazy(() => import('../screens/TopGames'));
+const Videos = lazy(() => import('../screens/Videos'));
 
 function Router() {
   return (
     <BrowserRouter>
       <>
         <NavBar />
-        <Switch>
-          <Route path='/videos/:userId' component={Videos} />
-          <Route
-            path='/directory/game/:gameId/channel/:userId'
-            component={LiveStream}
-          />
-          <Route path='/directory/game/:gameId' component={LiveStreams} />
-          <Route path='/directory' component={TopGames} />
-          <Route path='/' component={Home} />
-        </Switch>
+        <Suspense fallback={<Loader fullscreen />}>
+          <Switch>
+            <Route path='/videos/:userId' component={Videos} />
+            <Route
+              path='/directory/game/:gameId/channel/:userId'
+              component={LiveStream}
+            />
+            <Route path='/directory/game/:gameId' component={LiveStreams} />
+            <Route path='/directory' component={TopGames} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </Suspense>
       </>
     </BrowserRouter>
   );
