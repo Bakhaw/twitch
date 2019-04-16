8918d1e8f1fe0be86185b07c313a60c127472442
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-04-16 15:37:52 +0200

    :art: on <PrevButton />

--- src/components/PrevButton/index.js
+++ src/components/PrevButton/index.js
@@ -8,21 +8,20 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 26px;
-  width: 26px;
+  height: 22px;
+  width: 22px;
   background: rgba(0, 0, 0, 0.3);
   border-radius: 50%;
   padding: 5px;
 `;
 
 const Icon = styled.img`
-  height: 24px;
-  width: 24px;
+  height: 18px;
 `;
 
 function PrevButton({ to }) {
   return (
-    <Wrapper>
+    <Wrapper className='PrevButton'>
       <Link to={to}>
         <Icon alt='Go back button' src={LeftArrowIcon} />
       </Link>
