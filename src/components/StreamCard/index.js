45046428bfe1417f41c5d352e4195444324603ac
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-04-17 15:12:10 +0200

    add a rule to handle "undefinded" erros in <StreamCard />

--- src/components/StreamCard/index.js
+++ src/components/StreamCard/index.js
@@ -5,12 +5,16 @@ import { Link } from 'react-router-dom';
 import Description from './Description';
 import Media from './Media';
 
-const StreamCard = ({ stream }) => (
-  <Link to={`/directory/game/${stream.game_id}/channel/${stream.user_id}`}>
-    <Media stream={stream} />
-    <Description stream={stream} />
-  </Link>
-);
+const StreamCard = ({ stream }) => {
+  if (!stream) return null;
+
+  return (
+    <Link to={`/directory/game/${stream.game_id}/channel/${stream.user_id}`}>
+      <Media stream={stream} />
+      <Description stream={stream} />
+    </Link>
+  );
+};
 
 StreamCard.propTypes = {
   stream: PropTypes.object
