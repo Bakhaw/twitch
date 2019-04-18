1017e75dec948b9fad5821908acd2b536ce5af48
Faïssal <faissal@MacBook-Pro-de-Faissal.local>
2019-04-18 19:00:49 +0200

    handle errors in custom useFetch hook

--- src/api/hooks.js
+++ src/api/hooks.js
@@ -4,8 +4,9 @@ import API from './index';
 export function useFetch(method, methodParams = [], initialState = []) {
   const [data, setData] = useState(initialState);
   const fetchData = async () => {
-    const { data } = await API[method](...methodParams);
-    await setData(data);
+    const request = await API[method](...methodParams);
+    if (!request) return;
+    await setData(request.data);
   };
 
   useEffect(() => {
